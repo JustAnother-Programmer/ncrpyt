4515738f4cac60921a9725c0fa6ce1cd360c1d60
JustAnother-Programmer <60672202+JustAnother-Programmer@users.noreply.github.com>
2021-03-20 11:00:08 +0000

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,57 @@
+const yargs = require('yargs')
+const ncrypt = require('./utils/ncrpyt.js')
+
+yargs.command({
+    command: 'setKey',
+    describe: 'Set the AES256 key.',
+    builder: {
+        key: {
+            describe: 'AES256 Key',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        ncrypt.setKey(argv.key)
+    }
+})
+
+yargs.command({
+    command: 'encrypt',
+    describe: 'Encrypt given text',
+    builder: {
+        text: {
+            describe: 'Text to be encrypted',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        ncrypt.encrypt(argv.text)
+    }
+})
+
+yargs.command({
+    command: 'getKey',
+    describe: 'Gets the current AES256 key',
+    handler() {
+        ncrypt.getKey()
+    }
+})
+
+yargs.command({
+    command: 'decrypt',
+    describe: 'Takes an encrypted message to decrypt. (MUST BE USING THE SAME KEY)',
+    builder: {
+        text: {
+            describe: 'Input the encrypted message',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        ncrypt.decrypt(argv.text)
+    }
+})
+
+yargs.parse()
---
+++ src/utils/ncrpyt.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const aes256 = require('aes256')
+
+var key = 'defaultkeychangeme'
+
+const encrypt = (text) => {
+    var encryptedText = aes256.encrypt(key, text)
+    fs.appendFileSync('../out/encrypts.txt', encryptedText + '\n')
+    console.log(chalk.red(`Your encrypted message: ${encryptedText}`))
+}
+
+const decrypt = (text) => {
+    var decryptedText = aes256.decrypt(key, text)
+    fs.appendFileSync('../out/decrypts.txt', decryptedText + '\n')
+    console.log(chalk.green(`Your decrypted message: ${decryptedText}`))
+}
+
+const setKey = (newKey) => {
+    key = newKey
+    console.log(chalk.blue(`Key is now: ${key}`))
+}
+
+const getKey = () => {
+    console.log(chalk.blue(`Your current key: ${key}`))
+}
+
+module.exports = {
+    getKey: getKey,
+    setKey: setKey,
+    decrypt: decrypt,
+    encrypt: encrypt
+}
