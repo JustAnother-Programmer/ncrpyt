f7df23816057854a446f316ce726715d4ed95a0c
StonkSelected <billydigby03@gmail.com>
2021-03-26 20:47:00 +0000

    Added some fancy titles to the cmd prompts

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
+title ncrpyt installer
 echo @echo off > run.bat
+echo title ncrpyt tool by JustAnother-Programmer >> run.bat
 echo cd src >> run.bat
 echo cmd /k node main.js listcmds >> run.bat
 echo @echo on >> run.bat
