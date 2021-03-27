db8171d13e2101aaaefd6badfd4573f4078593eb
StonkSelected <billydigby03@gmail.com>
2021-03-27 11:29:13 +0000

    Much better writing of batch files

--- install.bat
+++ install.bat
@@ -1,10 +1,12 @@
 @echo off
 title ncrpyt installer
-echo @echo off> run.bat
-echo title ncrpyt tool by JustAnother-Programmer>> run.bat
-echo cd src>> run.bat
-echo cmd /k node main.js listcmds>> run.bat
-echo @echo on>> run.bat
+(
+  echo @echo off
+  echo title ncrpyt tool by JustAnother-Programmer
+  echo cd src
+  echo cmd /k node main.js listcmds
+  echo @echo on
+) > run.bat
 cd src
 npm install
 @echo on
