13283b508a82859ac4d92e53b3e55576c936fd1e
StonkSelected <billydigby03@gmail.com>
2021-03-27 11:11:30 +0000

    More bat stuff

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 @echo off
 title ncrpyt installer
-echo @echo off > run.bat
-echo title ncrpyt tool by JustAnother-Programmer >> run.bat
-echo cd src >> run.bat
-echo cmd /k node main.js listcmds >> run.bat
-echo @echo on >> run.bat
+echo @echo off> run.bat
+echo title ncrpyt tool by JustAnother-Programmer>> run.bat
+echo cd src>> run.bat
+echo cmd /k node main.js listcmds>> run.bat
+echo @echo on>> run.bat
 cd src
 npm install
 @echo on
